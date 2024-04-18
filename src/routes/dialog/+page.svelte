019c04399d946aa55917158bc3729e0fb638909b
jonas <jonas.voisard@gmail.com>
2024-04-18 15:15:07 +0200

    doc: add dialogConfirm

--- src/routes/dialog/+page.svelte
+++ src/routes/dialog/+page.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
 	import Meta from '$lib/Meta.svelte'
-	import { Dialog } from '$lib/index.js'
+	import { Dialog, DialogConfirm } from '$lib/index.js'
 
-	let dialog: HTMLDialogElement
-	let component: Dialog
+	let dialogComponent: Dialog
+	let dialogElement: HTMLDialogElement
+
+	let dialogConfirmComponent: DialogConfirm
 </script>
 
-<Meta {component} name="Dialog">
-	<button class="btn" on:click={() => dialog.showModal()}>Ouvrir dialog</button>
+<Meta component={dialogComponent} name="Dialog">
+	<button class="btn" on:click={() => dialogElement.showModal()}>Ouvrir dialog</button>
 
-	<Dialog bind:this={component} bind:dialog>
+	<Dialog bind:this={dialogComponent} bind:dialog={dialogElement}>
 		<div slot="header">slot="header"</div>
 		slot
 		<div slot="footer">slot="footer"</div>
 	</Dialog>
+</Meta>
+
+<Meta component={dialogConfirmComponent} name="DialogConfirm">
+	<DialogConfirm bind:this={dialogConfirmComponent}>
+		<div slot="activator">slot="activator"</div>
+		<div slot="header">slot="header"</div>
+		slot
+		<div slot="action">slot="action"</div>
+	</DialogConfirm>
 </Meta>
