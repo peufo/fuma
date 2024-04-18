25ffc862d71dbacd7ca143b8f7c0ed56dcc06283
jonas <jonas.voisard@gmail.com>
2024-04-18 12:08:00 +0200

    style: better icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.pcss'
-	import { ModeWatcher, mode } from 'mode-watcher'
+
 	import Tree from './Tree.svelte'
 	import ToggleMode from './ToggleMode.svelte'
 
@@ -8,17 +8,9 @@
 		.map((p) => p.replace('./', '').replace(/\/?\+page.svelte/, ''))
 		.filter(Boolean)
 		.map((link) => link.split('/'))
-
-	mode.subscribe((_mode) => {
-		const [html] = document.getElementsByTagName('html')
-		if (!html || !_mode) return
-		html.setAttribute('data-theme', _mode)
-	})
 </script>
 
-<ModeWatcher />
-
-<div class="flex gap-4" data-theme={$mode} data-tamere="prout">
+<div class="flex gap-4">
 	<div class="menu flex flex-col p-4 min-h-full w-min">
 		<div class="flex items-center gap-4 mb-4">
 			<h2 class="title min-w-36">Mat</h2>
--- src/routes/ToggleMode.svelte
+++ src/routes/ToggleMode.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	import { toggleMode, mode } from 'mode-watcher'
+	import { browser } from '$app/environment'
+	import { ModeWatcher, toggleMode, mode } from 'mode-watcher'
+
 	import { Icon } from '$lib/index.js'
-	import { mdiMoonNew, mdiSunSnowflake } from '@mdi/js'
+	import { mdiWeatherNight, mdiWhiteBalanceSunny } from '@mdi/js'
+
+	mode.subscribe((_mode) => {
+		if (!browser) return
+		const [html] = document.getElementsByTagName('html')
+		if (!html || !_mode) return
+		html.setAttribute('data-theme', _mode)
+	})
 </script>
 
+<ModeWatcher />
+
 <button class="btn btn-square btn-sm" on:click={toggleMode}>
-	<Icon path={$mode === 'light' ? mdiSunSnowflake : mdiMoonNew} />
+	<Icon path={$mode === 'light' ? mdiWhiteBalanceSunny : mdiWeatherNight} />
 </button>
