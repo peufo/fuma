91f4586a66574dad414a5f37f1d33830248fa12e
jonas <jonas.voisard@gmail.com>
2024-04-24 14:21:24 +0200

    feat: customizable ToggleMode

--- src/lib/ui/ToggleMode.svelte
+++ src/lib/ui/ToggleMode.svelte
@@ -11,10 +11,14 @@
 		if (!html || !_mode) return
 		html.setAttribute('data-theme', _mode)
 	})
+
+	$: path = $mode === 'light' ? mdiWhiteBalanceSunny : mdiWeatherNight
 </script>
 
 <ModeWatcher />
 
-<button class="btn btn-square btn-sm" on:click={toggleMode}>
-	<Icon path={$mode === 'light' ? mdiWhiteBalanceSunny : mdiWeatherNight} />
-</button>
+<slot {toggleMode} {path}>
+	<button class="btn btn-square btn-sm" on:click={toggleMode}>
+		<Icon {path} />
+	</button>
+</slot>
