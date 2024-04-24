67265cebc0dca042261a54867d9322a9f2ab094c
jonas <jonas.voisard@gmail.com>
2024-04-24 15:44:19 +0200

    style: boolean input as vertical label

--- src/lib/ui/input/InputBoolean.svelte
+++ src/lib/ui/input/InputBoolean.svelte
@@ -3,30 +3,30 @@
 	import { FormControl, type InputProps, bindCheckedWithParams } from './index.js'
 
 	type $$Props = InputProps<boolean>
-	$: ({ input, value: _value, bindWithParams, label, ...props } = $$props as $$Props)
+	$: ({ input, value: _value, bindWithParams, label, ...props } = $$restProps as $$Props)
 	export let value = _value
 
 	const dispatch = createEventDispatcher<{ change: boolean }>()
 </script>
 
 <FormControl {...props} let:key>
-	<div class="flex items-center gap-2">
-		<input type="hidden" name={key} value={value ? 'true' : ''} />
-		<input
-			bind:checked={value}
-			use:bindCheckedWithParams={{ bindEnable: bindWithParams }}
-			on:input={({ currentTarget: { checked } }) => dispatch('change', checked)}
-			on:focus
-			on:blur
-			type="checkbox"
-			id={key}
-			class="checkbox"
-			{...input}
-		/>
-		<label for={key} class="label cursor-pointer">
-			<span class="label-text">
-				<slot>{label}</slot>
-			</span>
-		</label>
-	</div>
+	<label for={key} class="label cursor-pointer">
+		<span class="label-text">
+			<slot>{label}</slot>
+		</span>
+	</label>
+
+	<input
+		bind:checked={value}
+		use:bindCheckedWithParams={{ bindEnable: bindWithParams }}
+		on:input={({ currentTarget: { checked } }) => dispatch('change', checked)}
+		on:focus
+		on:blur
+		type="checkbox"
+		id={key}
+		class="checkbox ml-1"
+		{...input}
+	/>
+
+	<input type="hidden" name={key} value={value ? 'true' : ''} />
 </FormControl>
---
+++ src/routes/ui/form/inputs/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Meta from '$lib/Meta.svelte'
+	import { InputText, InputBoolean } from '$lib/ui/index.js'
+	import { useForm } from '$lib/validation/form.js'
+
+	const { enhance } = useForm()
+
+	let inputBoolean: InputBoolean
+	let inputText: InputText
+</script>
+
+<Meta component={inputText}>
+	<form action="" method="post" use:enhance>
+		<InputText bind:this={inputText} label="Text input" key="text" />
+	</form>
+</Meta>
+
+<Meta component={inputBoolean} name="InputBoolean">
+	<InputBoolean bind:this={inputBoolean} label="Boolean input" key="boolean" value={true} />
+</Meta>
