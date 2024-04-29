04895c24dc0f6d0afc4736c0ce3a21a98c630d6a
jonas <jonas.voisard@gmail.com>
2024-04-30 00:59:45 +0200

    feat: icon path as prop

--- src/lib/ui/ButtonCopy.svelte
+++ src/lib/ui/ButtonCopy.svelte
@@ -6,6 +6,7 @@
 
 	export let value: string
 	export let title: string
+	export let path = mdiClipboardTextOutline
 	export let successMessage = 'Copied'
 
 	function handleClick() {
@@ -21,5 +22,5 @@
 </script>
 
 <button on:click|preventDefault={handleClick} class="btn btn-square btn-ghost btn-sm">
-	<Icon path={mdiClipboardTextOutline} {title} size={22} />
+	<Icon {path} {title} size={22} />
 </button>
