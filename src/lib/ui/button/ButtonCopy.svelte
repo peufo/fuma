ac3b0dc737d7d40de6f9b2fde0b22dc930cfb9f9
jonas <jonas.voisard@gmail.com>
2024-05-13 11:56:22 +0200

    feat: ButtonCopy handle promise

--- src/lib/ui/button/ButtonCopy.svelte
+++ src/lib/ui/button/ButtonCopy.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
 	import { mdiClipboardTextOutline } from '@mdi/js'
 	import { toast } from 'svelte-sonner'
-
 	import { Icon } from '$lib/ui/icon/index.js'
-
-	export let value: string
-	export let title: string
+	let valueOrGetValue: string | (() => Promise<string>)
+	export { valueOrGetValue as value }
+	export let title = ''
 	export let icon = mdiClipboardTextOutline
 	export let successMessage = 'Copied'
 
-	function handleClick() {
+	let isLoading = false
+
+	async function loadValue(): Promise<string> {
+		if (typeof valueOrGetValue === 'string') return valueOrGetValue
+		return valueOrGetValue()
+	}
+
+	async function handleClick() {
+		if (isLoading) return
+		isLoading = true
+		const value = await loadValue().finally(() => (isLoading = false))
+
 		navigator.clipboard
 			.writeText(value)
 			.then(() => {
@@ -21,6 +31,20 @@
 	}
 </script>
 
-<button on:click|preventDefault={handleClick} class="btn btn-square btn-ghost btn-sm">
-	<Icon path={icon} {title} size={22} />
-</button>
+<div class="relative">
+	{#if isLoading}
+		<span class="loading loading-spinner absolute left-1 top-1 scale-125 opacity-25" />
+	{/if}
+	<button
+		class="btn btn-square btn-sm"
+		on:click|preventDefault={handleClick}
+		class:btn-disabled={isLoading}
+	>
+		<Icon
+			path={icon}
+			size={20}
+			{title}
+			class="transition-transform {isLoading ? 'scale-75' : ''}"
+		/>
+	</button>
+</div>
--- src/routes/ui/button/+page.svelte
+++ src/routes/ui/button/+page.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
+	import { getCSV } from '$lib/index.js'
 	import Meta from '$lib/private/Meta.svelte'
 	import { ButtonCopy, ButtonDelete } from '$lib/ui/button/index.js'
+	import { mdiDownload } from '@mdi/js'
 
 	let buttonCopy: ButtonCopy
 	let buttonDelete: ButtonDelete
+
+	async function getValue() {
+		await wait(1500)
+		const users = [
+			{ name: 'Bob', age: 24 },
+			{ name: 'Alice', age: 42 }
+		]
+
+		return getCSV(users, { name: (user) => user.name, age: (user) => user.age })
+	}
+
+	async function wait(ms: number) {
+		return new Promise((resolve) => {
+			setTimeout(resolve, ms)
+		})
+	}
 </script>
 
 <Meta component={buttonCopy} name="ButtonCopy">
-	<ButtonCopy bind:this={buttonCopy} title="Copy me !" value="Hey 👋" />
+	<div class="mx-auto flex gap-2">
+		<ButtonCopy bind:this={buttonCopy} title="Copy me !" value="Hey 👋" />
+		<ButtonCopy title="Copy async data" value={getValue} icon={mdiDownload} />
+	</div>
 </Meta>
 
 <Meta component={buttonDelete} name="ButtonDelete" description="Doit être placé dans un <form />">
