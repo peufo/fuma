cba59d8bebb77d6a3816ea2f29aa14e68705a4d2
jonas <jonas.voisard@gmail.com>
2024-05-13 11:56:12 +0200

    feat: add getCSV in utils

---
+++ src/lib/utils/csv.ts
@@ -0,0 +1,17 @@
+export type CSVFields<Item extends Record<string, unknown>> = Partial<
+	Record<keyof Item, (item: Item) => string | number>
+>
+
+export function getCSV<Item extends Record<string, unknown>>(
+	items: Item[],
+	fields: CSVFields<Item>
+): string {
+	const headers = Object.keys(fields).join('\t')
+	const rows = items.map((m: Item) =>
+		Object.values(fields)
+			.filter((getValue) => !!getValue)
+			.map((getValue) => getValue(m))
+			.join('\t')
+	)
+	return [headers, rows.join('\r\n')].join('\r\n')
+}
