b230a3920fe624e382a106081fbb9ce6b0fb6a23
jonas <jonas.voisard@gmail.com>
2024-05-16 13:41:03 +0200

    add color on exemple

--- src/routes/ui/list/+page.svelte
+++ src/routes/ui/list/+page.svelte
@@ -3,14 +3,18 @@
 	import Icon from '$lib/ui/icon/Icon.svelte'
 	import { mdiDrag } from '@mdi/js'
 
-	let items = Array(10)
+	let items = Array(16)
 		.fill(0)
-		.map((v, i) => `Item ${i}`)
+		.map((v, i) => ({
+			label: `Item ${i}`,
+			color: `rgb(255, ${255 - i * 10}, ${255 - i * 20})`,
+			key: i
+		}))
 </script>
 
-<div class="flex items-start justify-evenly gap-4">
+<div class="mt-10 flex items-start justify-evenly gap-4">
 	<ul
-		class="min-w-40 rounded border"
+		class="min-w-40 overflow-scroll rounded border p-1"
 		use:listEditable={{
 			items,
 			onChange(newOrder) {
@@ -18,13 +22,15 @@
 			}
 		}}
 	>
-		{#each items as item}
-			<li class="menu-item">{item}</li>
+		{#each items as item (item.key)}
+			<li class="menu-item" style="background: {item.color};">
+				{item.label}
+			</li>
 		{/each}
 	</ul>
 
 	<ul
-		class="min-w-40 rounded border"
+		class="flex h-80 min-w-40 flex-col gap-1 overflow-auto rounded-2xl border p-1"
 		use:listEditable={{
 			items,
 			onChange(newOrder) {
@@ -33,9 +39,9 @@
 			dragElementsSelector: '.drag-button'
 		}}
 	>
-		{#each items as item}
-			<li class="menu-item">
-				<span> {item}</span>
+		{#each items as item (item.key)}
+			<li class="menu-item" style="background: {item.color};">
+				<span>{item.label}</span>
 				<Icon path={mdiDrag} class="drag-button btn btn-square btn-ghost btn-sm ml-auto" />
 			</li>
 		{/each}
