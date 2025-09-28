b708544c38c1ce81384128bf5225e895e5020a3b
jonas <jonas.voisard@gmail.com>
2025-09-28 17:01:00 +0200

    upgrade zod

--- src/lib/server/parseQuery.ts
+++ src/lib/server/parseQuery.ts
@@ -1,8 +1,7 @@
-import type { ZodRawShape } from 'zod'
+import z from 'zod'
 import { error } from '@sveltejs/kit'
-import { z } from '$lib/validation/zod.js'
 
-export function parseQuery<Type extends ZodRawShape>(url: URL, shape: Type) {
+export function parseQuery<Shape extends z.core.$ZodShape>(url: URL, shape: Shape) {
 	const queryRaw: Record<string, unknown> = {}
 	Object.keys(shape).forEach((name) => {
 		const param = url.searchParams.get(name)
