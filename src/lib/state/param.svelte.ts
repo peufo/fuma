5ab52751f6b4f489fa5283682f972083744930a1
jonas <jonas.voisard@gmail.com>
2026-02-06 19:02:11 +0100

    fix: param and urlParam (don't use $derived in *.svelte.ts)

--- src/lib/state/param.svelte.ts
+++ src/lib/state/param.svelte.ts
@@ -1,37 +1,55 @@
 import { page } from '$app/state'
 
-export let param = $derived({
-	with(params: Record<string, string | number>, ...keysToRemove: string[]) {
-		const url = new URL(page.url)
-		Object.entries(params).forEach(([key, value]) => url.searchParams.set(key, String(value)))
-		keysToRemove.forEach((key) => url.searchParams.delete(key))
-		return url.search
+export const param = {
+	get with() {
+		return (params: Record<string, string | number>, ...keysToRemove: string[]) => {
+			const url = new URL(page.url)
+			Object.entries(params).forEach(([key, value]) => url.searchParams.set(key, String(value)))
+			keysToRemove.forEach((key) => url.searchParams.delete(key))
+			return url.search
+		}
 	},
-	without(...keys: string[]) {
-		const url = new URL(page.url)
-		keys.forEach((key) => url.searchParams.delete(key))
-		return url.search
+	get without() {
+		return (...keys: string[]) => {
+			const url = new URL(page.url)
+			keys.forEach((key) => url.searchParams.delete(key))
+			return url.search
+		}
 	},
-	toggle(params: Record<string, string>, ...keysToRemove: string[]) {
-		const url = new URL(page.url)
-		Object.entries(params).forEach(([key, value]) => {
-			if (url.searchParams.get(key) === value) url.searchParams.delete(key)
-			else url.searchParams.set(key, value)
-		})
-		keysToRemove.forEach((key) => url.searchParams.delete(key))
-		return url.search
+	get toggle() {
+		return (params: Record<string, string>, ...keysToRemove: string[]) => {
+			const url = new URL(page.url)
+			Object.entries(params).forEach(([key, value]) => {
+				if (url.searchParams.get(key) === value) url.searchParams.delete(key)
+				else url.searchParams.set(key, value)
+			})
+			keysToRemove.forEach((key) => url.searchParams.delete(key))
+			return url.search
+		}
 	},
-	has: (key: string) => page.url.searchParams.has(key),
-	get: (key: string) => page.url.searchParams.get(key),
-	hasValue: (key: string, value: string) => page.url.searchParams.get(key) === value,
-})
+	get has() {
+		return ((key: string) => page.url.searchParams.has(key))
+	},
+	get get() {
+		return (key: string) => page.url.searchParams.get(key)
+	},
+	get hasValue() {
+		return (key: string, value: string) => page.url.searchParams.get(key) === value
+	}
+}
 
 function addPathname<Args extends unknown[]>(fn: (...args: Args) => string): ((...args: Args) => string) {
 	return (...args) => page.url.pathname + fn(...args)
 }
-export let urlParam = $derived({
+export const urlParam = {
 	...param,
-	with: addPathname(param.with),
-	without: addPathname(param.without),
-	toggle: addPathname(param.toggle),
-})
+	get with() {
+		return addPathname(param.with)
+	},
+	get without() {
+		return addPathname(param.without)
+	}, 
+	get toggle() {
+		return addPathname(param.toggle)
+	}
+}
