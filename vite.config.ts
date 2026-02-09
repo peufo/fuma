eb7068faf8651392554d5cc3e262bebb75aedb60
jonas <jonas.voisard@gmail.com>
2026-02-09 22:07:58 +0100

    add devtool-json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vitest/config'
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-})
+	plugins: [sveltekit(), tailwindcss(), devtoolsJson()],
+	test: { include: ['src/**/*.{test,spec}.{js,ts}'] }
+});
