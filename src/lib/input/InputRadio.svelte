9bb5bb49f13d01dda6780fcca746c261d9db02da
jonas <jonas.voisard@gmail.com>
2026-07-31 16:18:19 +0200

    fix: bad render radio input

--- src/lib/input/InputRadio.svelte
+++ src/lib/input/InputRadio.svelte
@@ -2,6 +2,7 @@
 	import type { RemoteFormField } from '@sveltejs/kit'
 	import { type Options, parseOptions } from '$lib/utils/index.js'
 	import Issues from './Issues.svelte'
+	import { CheckIcon } from '@lucide/svelte'
 
 	let {
 		field,
@@ -23,26 +24,47 @@
 	<div class="join join-vertical">
 		{#each options as option (option.value)}
 			{@const { class: klass, ...props } = option}
-			<label class="input join-item not-disabled:cursor-pointer not-disabled:hover:bg-base-200">
-				{#if option.icon}
-					<option.icon size={18} opacity={0.8} />
-				{/if}
-				<span>{option.label}</span>
-				{#if field}
-					<input
-						{...field.as('radio', option.value)}
-						class={['radio ml-auto radio-sm', klass]}
-						{...props}
-					/>
-				{:else}
-					<input
-						type="radio"
-						checked={value === option.value}
-						onchange={() => (value = option.value)}
-						class={['radio ml-auto radio-sm', klass]}
-						{...props}
-					/>
-				{/if}
+			{@const checked = field?.value() === option.value || value === option.value}
+			<label
+				class={[
+					'input join-item not-disabled:cursor-pointer not-disabled:hover:bg-base-200',
+					'group h-auto min-h-10'
+				]}
+			>
+				<div class="flex h-(--size) grow items-center gap-2">
+					{#if option.icon}
+						<option.icon size={18} opacity={0.8} />
+					{/if}
+					<span class="grow">{option.label}</span>
+					{#if field}
+						<input {...field.as('radio', option.value)} class={['w-0']} {...props} />
+					{:else}
+						<input
+							type="radio"
+							{checked}
+							onchange={() => (value = option.value)}
+							class={['w-0']}
+							{...props}
+						/>
+					{/if}
+					<div
+						class={[
+							'ml-auto h-5 w-5 rounded-full outline',
+							'bg-base-100',
+							!checked && 'outline-base-content/20',
+							klass
+						]}
+					>
+						<div
+							class={[
+								'grid h-5 w-5 place-content-center rounded-full',
+								'bg-base-content',
+								'ease scale-0 opacity-0',
+								checked && 'scale-[80%] opacity-100'
+							]}
+						></div>
+					</div>
+				</div>
 			</label>
 		{/each}
 	</div>
@@ -50,7 +72,17 @@
 </div>
 
 <style>
-	input {
-		--input-color: var(--color-base-content);
+	.ease {
+		transition-property: translate background-color opacity;
+		transition-timing-function: cubic-bezier(0.275, 0.485, 0.515, 1.45);
+		transition-duration: 200ms;
+	}
+
+	label:has(input[aria-invalid='true']) {
+		--input-color: var(--color-error);
+	}
+	label:has(input:disabled) {
+		cursor: not-allowed;
+		border-color: var(--input-color);
 	}
 </style>
