64f487f8467af670dbf86f71d32bd2fee80be52e
jonas <jonas.voisard@gmail.com>
2026-08-14 19:39:40 +0200

    fix: style

--- src/lib/input/InputRange.svelte
+++ src/lib/input/InputRange.svelte
@@ -7,24 +7,27 @@
 		label,
 		field,
 		value = $bindable(),
+		name,
 		class: klass,
 		...props
 	}: {
 		label: string
 		field?: RemoteFormField<number>
 		value?: number
+		/** Sans `field`, ce qui nomme la valeur soumise. Avec, `field.as()` s'en charge. */
+		name?: string
 	} & InputProps = $props()
 </script>
 
 <label>
 	<div class="flex gap-2 px-3 text-sm">
 		<span class="label text-wrap">{label}</span>
-		<span class="ml-auto">{field?.value() ?? value}</span>
+		<span class="label ml-auto">{field?.value() ?? value}</span>
 	</div>
 	{#if field}
-		<input class={['range range-sm', klass]} {...field.as('range')} {...props} />
+		<input class={['range w-full range-sm', klass]} {...field.as('range')} {...props} />
 	{:else}
-		<input class={['range range-sm', klass]} type="range" bind:value {...props} />
+		<input class={['range w-full range-sm', klass]} type="range" {name} bind:value {...props} />
 	{/if}
 	<Issues {field} />
 </label>
